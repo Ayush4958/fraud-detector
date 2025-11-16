07d2020d9f8df9d6f61c85df0919ad2519981aaa
Ayush <bhandariayush935@gmail.com>
2025-11-16 23:35:16 +0530

    Source Files

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import Image from "next/image";
+import BackGround from '../components/BackGround'
 
 export default function Home() {
   return (
    <>
 
-    <div><h1>Welcome to fraud detector</h1></div>
-
+    <BackGround >
+      
+</BackGround>
    </>
   );
 }
