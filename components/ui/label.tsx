41db98bc1e08bbee3d8832ac36f2f221d3adaecb
Ayush <bhandariayush935@gmail.com>
2025-11-25 23:35:29 +0530

    components updated

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,8 +1,8 @@
-"use client";
-import * as React from "react";
-import * as LabelPrimitive from "@radix-ui/react-label";
+"use client"
+import * as React from "react"
+import * as LabelPrimitive from "@radix-ui/react-label"
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
@@ -12,11 +12,11 @@ const Label = React.forwardRef<
     ref={ref}
     className={cn(
       "text-sm font-medium text-black dark:text-white leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
-      className
+      className,
     )}
     {...props}
   />
-));
-Label.displayName = LabelPrimitive.Root.displayName;
+))
+Label.displayName = LabelPrimitive.Root.displayName
 
-export { Label };
+export { Label }
