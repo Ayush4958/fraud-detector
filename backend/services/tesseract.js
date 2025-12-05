d6dcbb6356c5a00212cc806b7b16e35035258407
Ayush <bhandariayush935@gmail.com>
2025-12-05 23:20:10 +0530

    OCR with tesseract

---
+++ backend/services/tesseract.js
@@ -0,0 +1,15 @@
+import Tesseract from "tesseract.js";
+
+export async function extractTextWithTesseract(fileBuffer) {
+
+  try {
+    const result = await Tesseract.recognize(fileBuffer, "eng");
+    return result.data.text;
+  } 
+
+  catch (error) {
+    console.error("Tesseract OCR Error:", error);
+    throw new Error("Failed to extract text (Tesseract)");
+  }
+
+}
