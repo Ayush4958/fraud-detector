972a1d46ec312cb06f8186ddbc9cbaf8075a3ec3
Ayush <bhandariayush935@gmail.com>
2025-12-05 23:31:46 +0530

    Updated dependecy

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
 import express from "express";
 import cors from "cors";
-import scanRoutes from "./routes/scan.routes.js";
+import scanRoutes from "./routes/scan.js";
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+
 app.use("/api", scanRoutes);
 
 export default app;
