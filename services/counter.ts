47806794664d1974d7b6d5ca767084a7786f5068
Ayush <bhandariayush935@gmail.com>
2025-12-24 22:56:35 +0530

    Services updated

--- services/counter.ts
+++ services/counter.ts
@@ -1,7 +1,13 @@
-export async function generateCounterBill(scanId: string) {
+export async function generateCounterBill(scanId: string, token: string) {
   const res = await fetch(
     `http://localhost:5000/api/counter-bill/${scanId}`,
-    { method: "POST" }
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      body: JSON.stringify({scanId : scanId})
+    },
   );
   return res.json();
 }
